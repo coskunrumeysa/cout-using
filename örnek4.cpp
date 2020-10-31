56ea3a3bab71703e2d0173933f592c98341435b6
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-10-31 14:16:35 +0300

    Add files via upload

---
+++ örnek4.cpp
@@ -0,0 +1,9 @@
+//Rümeysa Coþkun
+//Yine hello world ve hi ý am rumeysa ayný satýrda çýkacak burdaki amacým coutun baþka bir þekilde kullanýmýydý.
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Hello world!!!"<<"Hi ý am rumeysa";
+return 0;	
+}
